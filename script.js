a24b27318dc2e844aa0e56afa140110c6e39e6da
victoria41 <vika.rostomyan@mail.ru>
2020-10-30 18:16:25 +0400

    pn

---
+++ script.js
@@ -0,0 +1,12 @@
+let down = false
+document.getElementById("hov").addEventListener("mouseover", function(){
+    document.getElementById("dropmenu").style.display = "block"
+
+}) 
+
+document.getElementById("hov").addEventListener("mouseout", function(){
+    document.getElementById("dropmenu").style.display = "none"
+
+}) 
+
+
